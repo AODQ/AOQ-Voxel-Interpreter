4853afe1998e73155f5c240d33fbe214ee277eab
aodq <aodqig@gmail.com>
2017-04-03 11:51:24 -0400

    added more rules starting to eval

--- source/app.d
+++ source/app.d
@@ -1,26 +1,62 @@
 import std.stdio;
+import pegged.grammar;
+
+mixin(grammar(`
+  AOQ:
+    SExpr < "(" (Atom/SExpr)+ ")"
+
+    Sign < "-"
+    Integer <- digit+
+    IntegerL <- Sign? Integer
+    FloatL <- IntegerL "." Integer "f"
+    List < "'(" (Atom / SExpr / List)* ")"
+
+    Atom <- FloatL / IntegerL / Variable / List / Operator
+
+    Comment < "//" (!endOfLine .)* endOfLine
+
+    Variable <- (alpha / Alpha / Operator)+
+    Operator < "+" / "-" / "*" / "/" / "<" / ">" / ">=" / "<=" / "=" / "_"
+`));
+
+struct Atom {
+  string val;
+}
+
+Atom Eval ( ParseTree expr ) {
+  import std.algorithm, std.array;
+  foreach ( ref atom; expr.children ) {
+    switch ( atom.name ) {
+      default:
+        assert(false, "Unknown atom name: " ~ atom.name);
+      case "AOQ.Atom":
+        return Eval(atom.children[0]);
+      case "AOQ.Variable":
+        return Atom(atom.input);
+      case "AOQ.IntegerL":
+        return Atom(atom.input);
+      case "AOQ.FloatL":
+        return Atom(atom.input);
+      case "AOQ.SExpr":
+        Atom func = expr.children[0].Eval;
+        writeln("CHILD: ", expr.children[0].input);
+        auto args = expr.children[1 .. $].map!(Eval).array;
+        writeln("FUNC: ", func, " --- ", args);
+      break;
+    }
+  }
+  return Atom("");
+}
+
+auto Evaluate ( string str_expression ) {
+  auto expression = AOQ(str_expression);
+  writeln(expression);
+  writeln("---");
+  expression.Eval;
+  return "";
+}
 
 void main() {
-  import pegged.grammar;
-
-  mixin(grammar(`
-    AOQ:
-      Expr < Variable / IntegerLiteral / FloatLiteral / "(" Operator Expr+ ")"
-
-      Sign < "-"
-      Integer < digit+
-      IntegerLiteral < Sign? Integer
-      FloatLiteral < Sign? Integer "." Integer "f"
-
-      Comment < "//" (!endOfLine .)* endOfLine
-
-      Variable <- (alpha / Alpha / "_") (alpha / Alpha / "_" / digit)*
-
-      Operator < "+" / "-" / "*" / "/" / PredefinedFunctions
-
-      PredefinedFunctions < "set" / "lambda"
-  `));
-
-  auto parse_tree = AOQ("(set x (lambda (t y) (+ t y t y)))");
-  writeln(parse_tree);
+  "(+ 1 2)".Evaluate.writeln;
+  // "(+ '(1 2.0f) somevar)".Evaluate.writeln;
 }
